9c8fad1520febbece3d69a3d76d6906be843dafd
kartikjaiswal825 <kartikjaiswal825@gmail.com>
2024-11-24 23:28:47 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#6439FF',
+        'anuj': '#01ee91'
+      }, 
+      fontFamily: {
+        'display-font': ['poppins','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
